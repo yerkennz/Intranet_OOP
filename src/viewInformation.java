d875fc205def37b599e409e5abba2765ef3c820c
ZhandarbekCh <98944159+ZhandarbekCh@users.noreply.github.com>
2022-12-14 16:28:23 +0600

    Create viewInformation.java

---
+++ src/viewInformation.java
@@ -0,0 +1,16 @@
+public interface viewInformation {
+    
+    public void viewCourseFiles();
+
+   
+    public void viewAttendance();
+
+   
+    public void viewJournal();
+
+    
+    public void viewTranscript();
+
+   
+    public void viewForCourse();
+}
