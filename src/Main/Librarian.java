ba5a2fa3713aaa85aa6b4b320e40bf924717d860
madi1908 <98903470+madi1908@users.noreply.github.com>
2022-12-22 00:42:08 +0600

    Update Librarian.java

--- src/Main/Librarian.java
+++ src/Main/Librarian.java
@@ -18,14 +18,23 @@ public class Librarian extends Employee{
 		super(name,surname,id,birthDay,phoneNumber,email,workExperience);
 		allBook = new Vector<Book>();
 	}
+	/*
+	 * using this method librarian can see information about student
+	 */
 	public String showInfo() {
 		return "Name: "+this.getName()+"\nSurname: "+this.getSurname()+
 				"\nEmail: "+this.getEmail();
 	}
+	/*
+	 * librarian using this method adds book for library
+	 */
 	public void addBook(Genre genre, String title, String author,String id, int year) {
         Book bk = new Book(genre,author,title, id, year);
         allBook.add(bk);
     }
+	/*
+	 * librarian using this method removes book from library
+	 */
     public void removeBook(String id) {
         for (Book bk : allBook) {
             if(bk.getId().equals(id)) {
